0171f35376deb01fe3660ff9954359c711eb8506
Manuela Quintana <manuelaquintana28@gmail.com>
2025-03-17 22:55:02 -0500

    Lista Vacia

---
+++ Bucles_O_Ciclos/listavacia.py
@@ -0,0 +1,12 @@
+import random
+
+# Crear una lista vacía
+lista_numeros = []
+
+# Llenar la lista con 100 números aleatorios utilizando un bucle for
+for _ in range(100):
+    numero_aleatorio = random.randint(1, 1000)  # Genera un número aleatorio entre 1 y 1000
+    lista_numeros.append(numero_aleatorio)  # Agrega el número aleatorio a la lista
+
+# Mostrar la lista resultante
+print(lista_numeros)
