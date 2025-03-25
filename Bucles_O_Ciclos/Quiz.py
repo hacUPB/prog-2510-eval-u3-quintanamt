c27351dfc48c964bbb0669756d027fedeb60bb4a
Manuela Quintana <manuelaquintana28@gmail.com>
2025-03-25 15:37:02 -0500

    Reto

--- Bucles_O_Ciclos/Quiz.py
+++ Bucles_O_Ciclos/Quiz.py
@@ -4,22 +4,22 @@ stroke = 50
 print("Bienvenido al sistema de inventario. Stock disponible:", stroke)
 
 while stroke > 0: #Puse el while con 50 y era con Cero 
-    # Pedir cantidad al usuario
+   
     entrada = int(input("¿Cuántos productos quieres comprar? ")) #La entrada era dentro del ciclo while y en el quiz lo puse afuera
     
-    # Verificar si quiere salir  
+
     if entrada == 0: #No lo agregue en el código 
         print("Saliendo del sistema...")
         break #No lo agregue en el código 
     
-    # Verificar si hay suficiente stock
+  
     if entrada > stroke: #Puse el valor de stroke al inicial y no la variable como tal
         print("No hay suficientes productos disponibles.")
     else:
-        # Actualizar stock 
-        stroke = stroke - entrada #No puse esta parte 
+       
+        stroke = stroke - entrada #Lo puse en el orden equivocado en el quiz
         print("Compra realizada. Stock restante:", stroke)
         
-        # Verificar si se agotó el stock
+
         if stroke == 0: #No puse esta parte 
             print("Inventario agotado.")
